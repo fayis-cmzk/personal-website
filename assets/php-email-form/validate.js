3a3869b4459500e133c9052154b7d84cc02bbb12
Muhammed Fayis <fayisck098@gmail.com>
2021-03-24 16:30:31 +0530

    final commit

--- assets/php-email-form/validate.js
+++ assets/php-email-form/validate.js
@@ -124,42 +124,57 @@
   });
 
   function php_email_form_submit(this_form, action, data) {
+    // $.ajax({
+    //   type: "POST",
+    //   url: action,
+    //   data: data,
+    //   timeout: 40000
+    // }).done( function(msg){
+    //   if (msg.trim() == 'OK') {
+    //     this_form.find('.loading').slideUp();
+    //     this_form.find('.sent-message').slideDown();
+    //     this_form.find("input:not(input[type=submit]), textarea").val('');
+    //   } else {
+    //     this_form.find('.loading').slideUp();
+    //     if(!msg) {
+    //       msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
+    //     }
+    //     this_form.find('.error-message').slideDown().html(msg);
+    //   }
+    // }).fail( function(data){
+    //   console.log(data);
+    //   var error_msg = "Form submission failed!<br>";
+    //   if(data.statusText || data.status) {
+    //     error_msg += 'Status:';
+    //     if(data.statusText) {
+    //       error_msg += ' ' + data.statusText;
+    //     }
+    //     if(data.status) {
+    //       error_msg += ' ' + data.status;
+    //     }
+    //     error_msg += '<br>';
+    //   }
+    //   if(data.responseText) {
+    //     error_msg += data.responseText;
+    //   }
+    //   this_form.find('.loading').slideUp();
+    //   this_form.find('.error-message').slideDown().html(error_msg);
+    // });
     $.ajax({
-      type: "POST",
-      url: action,
-      data: data,
-      timeout: 40000
-    }).done( function(msg){
-      if (msg.trim() == 'OK') {
-        this_form.find('.loading').slideUp();
-        this_form.find('.sent-message').slideDown();
-        this_form.find("input:not(input[type=submit]), textarea").val('');
-      } else {
-        this_form.find('.loading').slideUp();
-        if(!msg) {
-          msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
-        }
-        this_form.find('.error-message').slideDown().html(msg);
+      url:"https://script.google.com/macros/s/AKfycbyMbV6naEpiz0TPd25VAizAVDe1b8vADPR45YVW/exec",
+      data:$("#submit-form").serialize(),
+      method:"post",
+      success:function (response){
+          alert("Your message recorded.Thankyou!!")
+          window.location.reload()
+          //window.location.href="https://google.com"
+      },
+      error:function (err){
+          alert("Something Error")
+
       }
-    }).fail( function(data){
-      console.log(data);
-      var error_msg = "Form submission failed!<br>";
-      if(data.statusText || data.status) {
-        error_msg += 'Status:';
-        if(data.statusText) {
-          error_msg += ' ' + data.statusText;
-        }
-        if(data.status) {
-          error_msg += ' ' + data.status;
-        }
-        error_msg += '<br>';
-      }
-      if(data.responseText) {
-        error_msg += data.responseText;
-      }
-      this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html(error_msg);
-    });
+  })
+
   }
 
 })(jQuery);
